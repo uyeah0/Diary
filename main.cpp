a73acafdfabc9873e1fb4a283b41c5639b93c666
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-08-20 23:51:44 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include<windows.h>
 #include<conio.h>
 #define MAGIC_KEY 224
-//#define UP 0
-//#define DOWN 1
-//#define LEFT 2
-//#define RIGHT 3
 #define SPACE 32
 #define UP 72
 #define DOWN 80
@@ -42,11 +38,6 @@ int keyControl() {
 
 		}
 	}
-	/*
-	if (temp == UP) { return UP; }
-	else if (temp == DOWN) { return DOWN; }
-	else if (temp == LEFT) { return LEFT; }
-	else if (temp == RIGHT) { return RIGHT; }*/
 }
 
 int menu() {
@@ -93,57 +84,13 @@ int menu() {
 	default:
 		return 0; break;
 	}
-	
-	//while (1) {
-	//	int n = keyControl();
-	//	switch (n)
-	//	{
-	//	case UP: {
-	//		if (y > 3) { // 3~7까지 이동
-	//			gotoxy(x - 2, y);
-	//			cout << " ";
-	//			gotoxy(x - 2, --y);
-	//			cout << ">";
-	//		}
-	//		break;
-	//	}
-	//	case DOWN: {
-	//		if (y < 7) { // 최대 7
-	//			cout << " ";
-	//			gotoxy(x - 1, ++y);
-	//			cout << ">";
-	//		}
-	//		break;
-	//	}
-	//	case SPACE: {
-	//		return y - 3;
-	//	}
-	//	}
-	//}
 }
 
 int main() {
 
 	int menuCode = menu();
 	cout << menuCode << endl;
-	/*cout << "나의 콘솔 다이어리" << endl << endl;
-	cout << "1. 일기쓰기" << endl;
-	cout << "2. 일기목록(보기, 수정, 삭제)" << endl;
-	cout << "3. 도움말" << endl;
-	cout << "4. 종료하기" << endl << endl;
-	 
-	int menu;
-	cout << "메뉴 입력 >> ";
-	cin >> menu;
-	switch (menu)
-	{
-	case 1:  cout << "1번" << endl;  break;
-	case 2:	cout << "2번" << endl;  break;
-	case 3 : cout << "3번" << endl;  break;
-	case 4 :cout << "4번" << endl;  break;
-	default:
-		break;
-	}*/
+
 
 	return 0;
 }
