4bf87804728531cba7ee8dfbb1451826702d001f
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-08-30 21:48:38 +0900

    210830

---
+++ Help.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Windows.h>
+#include "Help.h"	
+using namespace std;
+
+void Help::PrintHelp() {
+
+}
---
+++ Help.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+#include "Main.h"
+using namespace std;
+
+class Help{
+public :
+	void PrintHelp() {
+		Main main = Main();
+		cout << "[도움말]" << endl;
+		MovePosition(40, 6);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7); // 글씨 색을 바꿈
+		cout << "콘솔 일기장 입니다." << endl;
+		MovePosition(40, 8);
+		cout << "메뉴 1번을 통해 일기를 작성합니다. " << endl;
+		MovePosition(40, 10);
+		cout << "메뉴 2번을 통해 작성했던 일기를 불러옵니다." << endl;
+		MovePosition(40, 12);
+		cout << "메뉴 4번을 눌러 종료합니다." << endl;
+		MovePosition(40, 16);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+		cout << "메인 화면으로 돌아가려면 아무 키나 누르세요." << endl;
+	}
+	void MovePosition(int x, int y) {
+		
+	}
+}
