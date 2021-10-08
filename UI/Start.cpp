8a498d5394722ce177d5988f407c2011177ce2dc
Yeyoung Yu <79914479+uyeah0@users.noreply.github.com>
2021-10-08 22:59:25 +0900

    Add files via upload

---
+++ UI/Start.cpp
@@ -0,0 +1,46 @@
+﻿#include "Start.h"
+
+void Start::printStart() {
+	system("mode con:cols=170 lines=43");
+	Menu menu = Menu();
+
+	menu.MovePosition(1,1);
+	cout << "                          ,;**:.                            ";
+	menu.MovePosition(1, 2);
+	cout << "                       ~*-.,    ~*-                      ";
+	menu.MovePosition(1, 3);
+	cout << "                     .!,;~-~;;-   ,*.                       ";
+	menu.MovePosition(1, 4);
+	cout << "                    -; .*,          ;:                      ";
+	menu.MovePosition(1, 5);
+	cout << "          .--:;!;;~~: ,;             -:                     ";
+	menu.MovePosition(1, 6);
+	cout << "        !!.                           -;*=!:!*!;            ";
+	menu.MovePosition(1, 7);
+	cout << "      !- :~,:!.                                 ~:          ";
+	menu.MovePosition(1, 8);
+	cout << "    .! ;,~;                                       * ;,      ";
+	menu.MovePosition(1, 9);
+	cout << "    :  .:                                        -~! -.     ";
+	menu.MovePosition(1, 10);
+	cout << "   * * :                                         ~- ; !     ";
+	menu.MovePosition(1, 11);
+	cout << "  .~; !                                           -!: . ,   ";
+	menu.MovePosition(1, 12);
+	cout << "  ; !,                                            !! - ,~   ";
+	menu.MovePosition(1, 13);
+	cout << "  ! ,!                                            !: : ,,   ";
+}
+
+
+
+
+int main() {
+	system("cls");
+	Start start = Start();
+	start.printStart();
+
+	/*Main main = Main();
+	main.printMain();*/
+	return 0;
+}
