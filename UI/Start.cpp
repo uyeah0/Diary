f73f1e0c361b59ca34973ea6f2857e5666dbb840
Yeyoung Yu <79914479+uyeah0@users.noreply.github.com>
2021-10-18 22:29:15 +0900

    Add files via upload

--- UI/Start.cpp
+++ UI/Start.cpp
@@ -4,52 +4,45 @@ void Start::printStart() {
 	system("mode con:cols=170 lines=43");
 	Menu menu = Menu();
 
-	menu.MovePosition(1,1);
-	cout << "                          ,;**:.                            ";
-	menu.MovePosition(1, 2);
-	cout << "                       ~*-.,    ~*-                      ";
-	menu.MovePosition(1, 3);
-	cout << "                     .!,;~-~;;-   ,*.                       ";
-	menu.MovePosition(1, 4);
-	cout << "                    -; .*,          ;:                      ";
-	menu.MovePosition(1, 5);
-	cout << "          .--:;!;;~~: ,;             -:                     ";
-	menu.MovePosition(1, 6);
-	cout << "        !!.                           -;*=!:!*!;            ";
-	menu.MovePosition(1, 7);
-	cout << "      !- :~,:!.                                 ~:          ";
-	menu.MovePosition(1, 8);
-	cout << "    .! ;,~;                                       * ;,      ";
-	menu.MovePosition(1, 9);
-	cout << "    :  .:                                        -~! -.     ";
-	menu.MovePosition(1, 10);
-	cout << "   * * :                                         ~- ; !     ";
-	menu.MovePosition(1, 11);
-	cout << "  .~; !                                           -!: . ,   ";
-	menu.MovePosition(1, 12);
-	cout << "  ; !,                                            !! - ,~   ";
-	menu.MovePosition(1, 13);
-	cout << "  ! ,!                                            !: : ,,   ";
-	menu.MovePosition(1, 14);
-	cout << "  !  *                                            ;! ;,     ";
-	menu.MovePosition(1, 15);
-	cout << "  !  !                                             ! .      ";
-	menu.MovePosition(1, 16);
-	cout << "  ;                                                ,:       ";
-	menu.MovePosition(1, 17);
-	cout << "  :                                               * ~!-     ";
-	menu.MovePosition(1, 18);
-	cout << "   :                                                  ,;    ";
-	menu.MovePosition(1, 19);
-	cout << "   !                                                    ~   ";
+	menu.MovePosition(20,4);
+	cout << "   ___                                      _     ";
+	menu.MovePosition(20, 5);
+	cout << "  / __|    ___     __       _ _    ___     | |_   ";
+	menu.MovePosition(20, 6);
+	cout << "  \\__ \\   / -_)   / _|     | '_|  / -_)    |  _|  ";
+	menu.MovePosition(20, 7);
+	cout << "  |___/   \\___|   \\__|_   _|_|_   \\___|    _\\__|  ";
+	menu.MovePosition(20, 8);
+	cout << "_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| ";
+	menu.MovePosition(20, 9);
+	cout << "\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-'\"`-0-0-' ";
+	
+	
 
-	menu.MovePosition(1, 20);
-	cout << "   :                                                  ,;    ";
-	menu.MovePosition(1, 21);
-	cout << "   !                                                    ~   ";
-
-
-
+	menu.MovePosition(40, 13);
+	cout << "_______                                          ";
+	menu.MovePosition(40, 14);
+	cout << "\\  ___ `'.   .--.                                ";
+	menu.MovePosition(40, 15);
+	cout << " ' |--.\\  \\  |__|                .-.          .- ";
+	menu.MovePosition(40, 16);
+	cout << " | |    \\  ' .--.          .-,.--.\\ \\        / / ";
+	menu.MovePosition(40, 17);
+	cout << " | |     |  '|  |    __    |  .-. |\\ \\      / /  ";
+	menu.MovePosition(40, 18);
+	cout << " | |     |  ||  | .:--.'.  | |  | | \\ \\    / /   ";
+	menu.MovePosition(40, 19);
+	cout << " | |     ' .'|  |/ |   \\ | | |  | |  \\ \\  / /    ";
+	menu.MovePosition(40, 20);
+	cout << " | |___.' /' |  |`\" __ | | | |  '-    \\ `  /     ";
+	menu.MovePosition(40, 21);
+	cout << "/_______.'/  |__| .'.''| | | |         \\  /      ";
+	menu.MovePosition(40, 22);
+	cout << "\\_______|/       / /   | |_| |         / /       ";
+	menu.MovePosition(40, 23);
+	cout << "                 \ \._,\ '/|_|     |`-' /        ";
+	menu.MovePosition(40, 24);
+	cout << "                  `--'  `\"          '..'         ";
 
 }
 
@@ -59,10 +52,10 @@ void Start::printStart() {
 
 int main() {
 	system("cls");
-	Start start = Start();
-	start.printStart();
+	//Start start = Start();
+	//start.printStart();
 
-	/*Main main = Main();
-	main.printMain();*/
+	Main main = Main();
+	main.printMain();
 	return 0;
 }
